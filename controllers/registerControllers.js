384f187e60fe1976594c7a30070dea88c7b965bc
AliAlsada <ali-alsadah1941@hotmail.com>
2022-12-01 00:00:13 +0300

    hashing

--- controllers/registerControllers.js
+++ controllers/registerControllers.js
@@ -3,10 +3,10 @@ const bcrypt = require("bcryptjs");
 const db = require("./dbConnection");
 
 
-const registerAuth = (req, res) => {
+const registerAuth = async (req, res) => {
     //get the data from the user
     const {email, firstName, lastName, password, country, city, street, phone} = req.body
-    let hashedPassword = bcrypt.hash(password, 8);
+    let hashedPassword = await bcrypt.hash(password, 8);
 
     //here, i need to validate the registered information (email, phone) from the database
     //validate the data
@@ -23,38 +23,38 @@ const registerAuth = (req, res) => {
             return console.error(err.message);
         } 
         else if (row == undefined){  
-        //*****************insert in the customer table*****************
-        sql = `INSERT INTO CUSTOMER(email, Fname, Lname, phone, country, city, street)  VALUES (?,?,?,?,?,?,?)`;
+            //*****************insert in the customer table*****************
+            sql = `INSERT INTO CUSTOMER(email, Fname, Lname, phone, country, city, street)  VALUES (?,?,?,?,?,?,?)`;
 
-        db.run(sql,  [email, firstName, lastName, phone, country, city, street], (err) => {
-            if (err) return console.log(err.message);
-        });
+            db.run(sql,  [email, firstName, lastName, phone, country, city, street], (err) => {
+                if (err) return console.log(err.message);
+            });
 
-        //*****************insert in the account table and hash the password*****************
-        sql = `INSERT INTO ACCOUNT(email, password)  VALUES (?,?)`;
-        db.run(sql,  [email, hashedPassword], (err) => {
-            if (err) return console.log(err.message);
-        });
+            //*****************insert in the account table and hash the password*****************
+            sql = `INSERT INTO ACCOUNT(email, password)  VALUES (?,?)`;
+            db.run(sql,  [email, hashedPassword], (err) => {
+                if (err) return console.log(err.message);
+            });
 
-        /**
-        * join the two tables in order to get the auto incremented primary keys
-        * then insert them into user_account table
-        */
+            /**
+            * join the two tables in order to get the auto incremented primary keys
+            * then insert them into user_account table
+            */
 
-        sql = `SELECT a.account_id, c.customer_id FROM ACCOUNT a INNER JOIN CUSTOMER c ON a.email = c.email WHERE c.email = ?`;
+            sql = `SELECT a.account_id, c.customer_id FROM ACCOUNT a INNER JOIN CUSTOMER c ON a.email = c.email WHERE c.email = ?`;
 
-        db.get(sql, [email], (err, row) => {
-            if (err) return console.error(err.message);
-                
-            else{
-                sql = `INSERT INTO USER_ACCOUNT(account_id, customer_id)  VALUES (?,?)`;
-                db.run(sql,  [row.account_id, row.customer_id], (err) => {
-                    if (err) return console.log(err.message);
-                });
-            }});
+            db.get(sql, [email], (err, row) => {
+                if (err) return console.error(err.message);
+                    
+                else{
+                    sql = `INSERT INTO USER_ACCOUNT(account_id, customer_id)  VALUES (?,?)`;
+                    db.run(sql,  [row.account_id, row.customer_id], (err) => {
+                        if (err) return console.log(err.message);
+                    });
+                }});
 
-            //*****************after inserting the data in the database, redirect the user to the log in page.*****************
-            res.render("signUp", {message: 'User registered!'});
+                //*****************after inserting the data in the database, redirect the user to the log in page.*****************
+                res.render("signUp", {message: 'User registered!'});
         }
 
         else {
