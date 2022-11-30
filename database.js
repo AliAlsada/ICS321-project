f545c0fb57cf2b696cd7268310d146cd09472bc2
Ali <ali-alsadah1941@hotmail.com>
2022-11-30 13:52:28 +0300

    user registration, done

--- database.js
+++ database.js
@@ -277,23 +277,23 @@ sql = `CREATE TABLE IF NOT EXISTS TRUCK(
 
 
 
-sql = `SELECT * FROM CUSTOMER`
-db.all(sql, [], (err, rows) => {
-        if (err) return console.log(err.message);
-        rows.forEach((row) => {console.log(row)});
-});
+// sql = `SELECT * FROM CUSTOMER`
+// db.all(sql, [], (err, rows) => {
+//         if (err) return console.log(err.message);
+//         rows.forEach((row) => {console.log(row)});
+// });
 
-sql = `SELECT * FROM ACCOUNT`
-db.all(sql, [], (err, rows) => {
-        if (err) return console.log(err.message);
-        rows.forEach((row) => {console.log(row)});
-});
+// sql = `SELECT * FROM ACCOUNT`
+// db.all(sql, [], (err, rows) => {
+//         if (err) return console.log(err.message);
+//         rows.forEach((row) => {console.log(row)});
+// });
 
-sql = `SELECT * FROM USER_ACCOUNT`
-db.all(sql, [], (err, rows) => {
-        if (err) return console.log(err.message);
-        rows.forEach((row) => {console.log(row)});
-});
+// sql = `SELECT * FROM USER_ACCOUNT`
+// db.all(sql, [], (err, rows) => {
+//         if (err) return console.log(err.message);
+//         rows.forEach((row) => {console.log(row)});
+// });
 
 // db.all("select name from sqlite_master where type='table'", function (err, tables) {
 //         console.log(tables);
